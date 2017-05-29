1edbfc09d3d40ad39545bd8ae5edf2c045a45ed3
efendy <wicac@yahoo.com>
2017-05-29 10:20:38 +0700

    tutorial 3 final

--- Dockerfile
+++ Dockerfile
@@ -6,15 +6,21 @@ RUN apk add --update nodejs
 
 RUN mkdir -p /apps/oauth2_tutorial
 RUN mkdir -p /apps/oauth2_tutorial/models
+RUN mkdir -p /apps/oauth2_tutorial/controllers
 WORKDIR /apps/oauth2_tutorial
 
 COPY models /apps/oauth2_tutorial/models
+COPY controllers /apps/oauth2_tutorial/controllers
+
 COPY package.json .
 COPY server.js .
 
 RUN npm install --save express
 RUN npm install --save mongoose
 RUN npm install --save body-parser
+RUN npm install --save bcrypt-nodejs
+RUN npm install --save passport
+RUN npm install --save passport-http
 
 ENTRYPOINT ["node"]
 CMD ["server.js"]
--- controllers/beer.js
+++ controllers/beer.js
@@ -1,38 +1,8 @@
 // Load required packages
-var express = require('express');
-var mongoose = require('mongoose');
-var bodyParser = require('body-parser');
-var Beer = require('./models/beer');
-
-// Connect to the beerlocker MongoDB
-mongoose.connect('mongodb://192.168.99.100:27017/beerlocker');
-
-// Create our Express application
-var app = express();
-
-// Use the body-parser package in our application
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-
-// Use environment defined port or 3000
-var port = process.env.PORT || 3000;
-
-// Create our Express router
-var router = express.Router();
-
-// Initial dummy route for testing
-// http://localhost:3000/api
-router.get('/', function(req, res) {
-  res.json({ message: 'You are running dangerously low on beer!' }); 
-});
-
-// Create a new route with the prefix /beers
-var beersRoute = router.route('/beers');
+var Beer = require('../models/beer');
 
 // Create endpoint /api/beers for POSTS
-beersRoute.post(function(req, res) {
+exports.postBeers = function(req, res) {
   // Create a new instance of the Beer model
   var beer = new Beer();
 
@@ -48,10 +18,10 @@ beersRoute.post(function(req, res) {
 
     res.json({ message: 'Beer added to the locker!', data: beer });
   });
-});
+};
 
 // Create endpoint /api/beers for GET
-beersRoute.get(function(req, res) {
+exports.getBeers = function(req, res) {
   // Use the Beer model to find all beer
   Beer.find(function(err, beers) {
     if (err)
@@ -59,13 +29,10 @@ beersRoute.get(function(req, res) {
 
     res.json(beers);
   });
-});
-
-// Create a new route with the /beers/:beer_id prefix
-var beerRoute = router.route('/beers/:beer_id');
+};
 
 // Create endpoint /api/beers/:beer_id for GET
-beerRoute.get(function(req, res) {
+exports.getBeer = function(req, res) {
   // Use the Beer model to find a specific beer
   Beer.findById(req.params.beer_id, function(err, beer) {
     if (err)
@@ -73,10 +40,10 @@ beerRoute.get(function(req, res) {
 
     res.json(beer);
   });
-});
+};
 
 // Create endpoint /api/beers/:beer_id for PUT
-beerRoute.put(function(req, res) {
+exports.putBeer = function(req, res) {
   // Use the Beer model to find a specific beer
   Beer.findById(req.params.beer_id, function(err, beer) {
     if (err)
@@ -93,10 +60,10 @@ beerRoute.put(function(req, res) {
       res.json(beer);
     });
   });
-});
+};
 
 // Create endpoint /api/beers/:beer_id for DELETE
-beerRoute.delete(function(req, res) {
+exports.deleteBeer = function(req, res) {
   // Use the Beer model to find a specific beer and remove it
   Beer.findByIdAndRemove(req.params.beer_id, function(err) {
     if (err)
@@ -104,11 +71,4 @@ beerRoute.delete(function(req, res) {
 
     res.json({ message: 'Beer removed from the locker!' });
   });
-});
-
-// Register all our routes with /api
-app.use('/api', router);
-
-// Start the server
-app.listen(port);
-console.log('Insert beer on port ' + port);
+};
